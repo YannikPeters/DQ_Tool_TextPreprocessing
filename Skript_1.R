ad1726d1dcbc0b6da6b1678dc75b7125de546d62
YannikPeters <yannik.peters@gesis.org>
2024-10-29 10:31:22 +0100

    latest changes

--- Skript_1.R
+++ Skript_1.R
@@ -9,38 +9,56 @@ install.packages("polyglotr")
 install.packages("deeplr")
 install.packages("stringdist")
 install.packages("textTinyR")
+install.packages("text")
+
 
 rm(auth_key)
 
-install.packages("reticulate")
+textrpp_install()
 
-# Lade reticulate
+# Initialize the installed conda environment.
+# save_profile = TRUE saves the settings so that you don't have to run textrpp_initialize() after restarting R. 
+textrpp_initialize(save_profile = TRUE)
+
+translations_de_en <- text::textTranslate(
+  text = olympics_data_de$tweet_text,
+  model = "Helsinki-NLP/opus-mt-de-en"
+)
+
+library(text)
 library(reticulate)
 
-# Installiere die transformers Bibliothek in Python (falls nicht bereits geschehen)
-py_run_string("!pip install transformers")
+# Install text required python packages in a conda environment (with defaults).
+text::textrpp_install()
 
-# Importiere die transformers Bibliothek
-transformers <- import("transformers")
+# Show available conda environments.
+reticulate::conda_list()
 
-# Lade das Modell zur Korrektur
-model <- transformers$pipeline("fill-mask", model = "bert-base-uncased")
+# Initialize the installed conda environment.
+# save_profile = TRUE saves the settings so that you don't have to run textrpp_initialize() after restarting R. 
+text::textrpp_initialize(save_profile = TRUE)
 
-# Beispielhafter Twitter-Datensatz
-tweets <- data.frame(tweet_text = c(
-  "I love programing in R.", 
-  "Ths is a wnderful day!",
-  "R is grreat for data analysis."
+# Test so that the text package work.
+textEmbed("hello")
+
+
+# Beispiel-Daten
+example <- data.frame(text = c(
+  "RT @user: Check this out! https://example.com",
+  "Just a tweet with @mention and some amp text.",
+  "This is a clean tweet without links.",
+  "RT via @anotherUser: Visit http://example.org for more info!",
+  "Text with ampersand & some punctuation!!!"
 ))
 
-# Rechtschreibkorrektur für Tweets
-corrected_tweets <- sapply(tweets$tweet_text, function(tweet) {
-  # Nutze das Modell zur Korrektur des Textes
-  result <- model(tweet)
-  # Wandle das Ergebnis in einen lesbaren Text um (erster Vorschlag)
-  corrected <- result[[1]]$sequence
-  return(corrected)
-})
+# Bereinigung des Textes in einem Befehl 
+example$text <- example$text %>%
+gsub("(RT|via)((?:\\b\\W*@\\w+)+)", "", .) %>%
+  gsub("https?://\\S+", "", .) %>%
+  gsub("@\\w+", "", .) %>%
+  gsub("[\r\n]", " ", .) %>%
+  gsub("[[:punct:]]+", " ", .) %>%
+  gsub("\\s+", " ", .) %>%
+  trimws(.)
 
-# Ausgabe der korrigierten Tweets
-corrected_tweets
+example
